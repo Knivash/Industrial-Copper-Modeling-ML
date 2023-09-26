e4245bb5365cb7840726c658d38aa1515eba31e3
Knivash <136495926+Knivash@users.noreply.github.com>
2023-09-26 12:50:11 +0530

    Update Copper_UI.py

--- Copper_UI.py
+++ Copper_UI.py
@@ -158,5 +158,5 @@ with tab2:
         else:
             st.write('## :red[The status is Lost] ')
 
-st.write(f'<h6 style="color:rgb(0, 153, 153,0.35);">App Created by Balavignesh SS</h6>', unsafe_allow_html=True)
+st.write(f'<h6 style="color:rgb(0, 153, 153,0.35);">App Created by Nivash K </h6>', unsafe_allow_html=True)
 
